3c3a246a629642e65948d59d39b3e3f6d6457924
Kim <Kim@Kim-PC>
2013-12-22 19:51:34 +0900

    123

--- src/uos/codingsroom/securitymap/MapActivity.java
+++ src/uos/codingsroom/securitymap/MapActivity.java
@@ -8,6 +8,7 @@ import net.daum.mf.map.api.MapView;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.AlertDialog.Builder;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.DialogInterface.OnClickListener;
@@ -17,6 +18,7 @@ import android.util.Log;
 import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.ViewGroup.LayoutParams;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -40,6 +42,7 @@ public class MapActivity extends Activity implements
 	private MapReverseGeoCoder reverseGeoCoder = null;
 
 	private final String API_KEY="9ceae170ed20454fa4010dc4d4acb1ddf99f2113";
+	private final String DATA_KEY="1fbb384aa99b21afd8457926d076ce810df72716";
 	private final String TITLE="안전지도";
 	
     /** Called when the activity is first created. */
@@ -75,8 +78,7 @@ public class MapActivity extends Activity implements
 	public boolean onCreateOptionsMenu(Menu menu) {
 		super.onCreateOptionsMenu(menu);
 		
-		menu.add(0, MENU_MAP_TYPE, Menu.NONE, "MapType");
-		menu.add(0, MENU_MAP_MOVE, Menu.NONE, "Move");
+		menu.add(0, MENU_MAP_TYPE, Menu.NONE, "지도 종류");
 		menu.add(0, MENU_MAP_OVERLAY, Menu.NONE, "Overlay");
 		
 		return true;
@@ -90,8 +92,7 @@ public class MapActivity extends Activity implements
 		switch (itemId) {
 		case MENU_MAP_TYPE:
 		{
-			String hdMapTile = mapView.isHDMapTileEnabled()? "HD Map Tile Off" : "HD Map Tile On";
-			String[] mapTypeMenuItems = { "Standard", "Satellite", "Hybrid", hdMapTile, "Clear Map Tile Cache"};
+			String[] mapTypeMenuItems = {"일반", "위성", "하이브리드"};
 
 			Builder dialog = new AlertDialog.Builder(this);
 			dialog.setTitle("Map Type");
@@ -112,239 +113,6 @@ public class MapActivity extends Activity implements
 					case 2: // Hybrid
 					{
 						mapView.setMapType(MapView.MapType.Hybrid);
-					}
-						break;
-					case 3: // HD Map Tile On/Off
-					{
-						if (mapView.isHDMapTileEnabled()) {
-							mapView.setHDMapTileEnabled(false);
-						} else {
-							mapView.setHDMapTileEnabled(true);
-						}
-					}
-						break;
-					case 4: // Clear Map Tile Cache
-					{
-						MapView.clearMapTilePersistentCache();
-					}
-						break;
-					}
-				}
-
-			});
-			dialog.show();
-		}
-			return true;
-		
-		case MENU_MAP_MOVE:
-		{
-			String rotateMapMenu = mapView.getMapRotationAngle() == 0.0f? "Rotate Map 60" : "Unrotate Map";
-			String[] mapMoveMenuItems = { "Move to", "Zoom to", "Move and Zoom to", "Zoom In", "Zoom Out", rotateMapMenu};
-			Builder dialog = new AlertDialog.Builder(this);
-			dialog.setTitle("Map Type");
-			dialog.setItems(mapMoveMenuItems, new OnClickListener() {
-				@Override
-				public void onClick(DialogInterface dialog, int which) {
-					switch (which) {
-					case 0: // Move to
-					{
-						mapView.setMapCenterPoint(MapPoint.mapPointWithGeoCoord(37.53737528, 127.00557633), true);
-					}
-						break;
-					case 1: // Zoom to
-					{
-						mapView.setZoomLevel(4, true);
-					}
-						break;
-					case 2: // Move and Zoom to
-					{
-						mapView.setMapCenterPointAndZoomLevel(MapPoint.mapPointWithGeoCoord(33.41, 126.52), 9, true);
-					}
-						break;
-					case 3: // Zoom In
-					{
-						mapView.zoomIn(true);
-					}
-						break;
-					case 4: // Zoom Out
-					{
-						mapView.zoomOut(true);
-					}
-						break;
-					case 5: // Rotate Map 60, Unrotate Map
-					{
-						if (mapView.getMapRotationAngle() == 0.0f) {
-							mapView.setMapRotationAngle(60.0f, true);	
-						} else {
-							mapView.setMapRotationAngle(0.0f, true);
-						}
-					}
-						break;
-					}
-				}
-
-			});
-			dialog.show();			
-		}
-			return true;
-			
-		case MENU_MAP_OVERLAY:
-		{
-			String[] overlayMenuItems = { "Add POI Items", "Remove a POI Item", "Remove All POI Items", "Add Polyline1", "Add Polyline2", "Remove All Polylines"};
-			Builder dialog = new AlertDialog.Builder(this);
-			dialog.setTitle("Overlay");
-			dialog.setItems(overlayMenuItems, new OnClickListener() {
-				@Override
-				public void onClick(DialogInterface dialog, int which) {
-					switch (which) {
-					case 0: // Add POI Items
-					{
-						mapView.removeAllPOIItems();
-						
-						poiItem = new MapPOIItem();
-						poiItem.setItemName("City on a Hill");
-						poiItem.setMapPoint(MapPoint.mapPointWithGeoCoord(37.541889,127.095388));
-						poiItem.setMarkerType(MapPOIItem.MarkerType.BluePin);
-						poiItem.setShowAnimationType(MapPOIItem.ShowAnimationType.DropFromHeaven);
-						mapView.addPOIItem(poiItem);
-						
-						MapPOIItem poiItem2 = new MapPOIItem();
-						poiItem2.setItemName("압구정");
-						poiItem2.setMapPoint(MapPoint.mapPointWithGeoCoord(37.527896,127.036245));
-						poiItem2.setMarkerType(MapPOIItem.MarkerType.RedPin);
-						poiItem2.setShowAnimationType(MapPOIItem.ShowAnimationType.NoAnimation);
-//						poiItem2.setShowCalloutBalloonOnTouch(false);
-						poiItem2.setTag(153);
-						mapView.addPOIItem(poiItem2);
-						
-						MapPOIItem poiItem3 = new MapPOIItem();
-						poiItem3.setItemName("다음커뮤니케이션");
-						poiItem3.setUserObject(String.format("item%d", 3));
-						poiItem3.setMapPoint(MapPoint.mapPointWithGeoCoord(37.537229,127.005515));
-						poiItem3.setMarkerType(MapPOIItem.MarkerType.CustomImage);
-						poiItem3.setShowAnimationType(MapPOIItem.ShowAnimationType.SpringFromGround);
-						//poiItem3.setCustomImageResourceId(R.drawable.custom_poi_marker);
-						// if anchor point is not set, the bottom-center point of image will be set as anchor point
-						poiItem3.setCustomImageAnchorPointOffset(new MapPOIItem.ImageOffset(22,0));
-						mapView.addPOIItem(poiItem3);
-						
-						MapPOIItem poiItem4 = new MapPOIItem();
-						poiItem4.setItemName("서울숲");
-						poiItem4.setMapPoint(MapPoint.mapPointWithGeoCoord(37.545024,127.03923));
-						poiItem4.setMarkerType(MapPOIItem.MarkerType.YellowPin);
-						poiItem4.setShowAnimationType(MapPOIItem.ShowAnimationType.SpringFromGround);
-						poiItem4.setShowDisclosureButtonOnCalloutBalloon(false);
-						poiItem4.setDraggable(true);
-						poiItem4.setTag(276);
-						mapView.addPOIItem(poiItem4);
-						
-						mapView.fitMapViewAreaToShowAllPOIItems();
-					}
-						break;
-					case 1: // Remove a POI Item"
-					{
-						MapPOIItem poiItem = mapView.findPOIItemByTag(276);
-						if (poiItem != null) {
-							mapView.removePOIItem(poiItem);	
-						}
-					}
-						break;
-					case 2: // Remove All POI Items
-					{
-						mapView.removeAllPOIItems();
-					}
-						break;
-					case 3: // Add Polyline1
-					{
-						MapPolyline existingPolyline = mapView.findPolylineByTag(1000);
-						if (existingPolyline != null) {
-							mapView.removePolyline(existingPolyline);
-						}
-						
-						MapPolyline polyline1 = new MapPolyline();
-						polyline1.setTag(1000);
-						polyline1.setLineColor(Color.argb(128, 255, 51, 0));						
-						polyline1.addPoint(MapPoint.mapPointWithGeoCoord(37.537229,127.005515));
-						polyline1.addPoint(MapPoint.mapPointWithGeoCoord(37.545024,127.03923));
-						polyline1.addPoint(MapPoint.mapPointWithGeoCoord(37.527896,127.036245));
-						polyline1.addPoint(MapPoint.mapPointWithGeoCoord(37.541889,127.095388));
-						mapView.addPolyline(polyline1);
-						
-						mapView.fitMapViewAreaToShowPolyline(polyline1);
-					}
-						break;
-					case 4: // Add Polyline2
-					{
-						MapPOIItem existingPOIItemStart = mapView.findPOIItemByTag(10001);
-						if (existingPOIItemStart != null) {
-							mapView.removePOIItem(existingPOIItemStart);
-						}
-						
-						MapPOIItem existingPOIItemEnd = mapView.findPOIItemByTag(10002);
-						if (existingPOIItemEnd != null) {
-							mapView.removePOIItem(existingPOIItemEnd);
-						}
-						
-						MapPolyline existingPolyline = mapView.findPolylineByTag(2000);
-						if (existingPolyline != null) {
-							mapView.removePolyline(existingPolyline);
-						}
-						
-						MapPOIItem poiItemStart = new MapPOIItem();
-						poiItemStart.setItemName("Start");
-						poiItemStart.setTag(10001);
-						poiItemStart.setMapPoint(MapPoint.mapPointWithWCONGCoord(475334.0,1101210.0));
-						poiItemStart.setMarkerType(MapPOIItem.MarkerType.CustomImage);
-						poiItemStart.setShowAnimationType(MapPOIItem.ShowAnimationType.SpringFromGround);
-						poiItemStart.setShowCalloutBalloonOnTouch(false);
-						//poiItemStart.setCustomImageResourceId(R.drawable.custom_poi_marker_start);
-						poiItemStart.setCustomImageAnchorPointOffset(new MapPOIItem.ImageOffset(29,2));
-						mapView.addPOIItem(poiItemStart);
-						
-						MapPOIItem poiItemEnd = new MapPOIItem();
-						poiItemEnd.setItemName("End");
-						poiItemEnd.setTag(10001);
-						poiItemEnd.setMapPoint(MapPoint.mapPointWithWCONGCoord(485016.0,1118034.0));
-						poiItemEnd.setMarkerType(MapPOIItem.MarkerType.CustomImage);
-						poiItemEnd.setShowAnimationType(MapPOIItem.ShowAnimationType.SpringFromGround);
-						poiItemEnd.setShowCalloutBalloonOnTouch(false);
-						//poiItemEnd.setCustomImageResourceId(R.drawable.custom_poi_marker_end);
-						poiItemEnd.setCustomImageAnchorPointOffset(new MapPOIItem.ImageOffset(29,2));
-						mapView.addPOIItem(poiItemEnd);
-						
-						MapPolyline polyline2 = new MapPolyline(21);
-						polyline2.setTag(2000);
-						polyline2.setLineColor(Color.argb(128, 0, 0, 255));
-						polyline2.addPoint(MapPoint.mapPointWithWCONGCoord(475334.0,1101210.0));
-						polyline2.addPoint(MapPoint.mapPointWithWCONGCoord(474300.0,1104123.0));
-						polyline2.addPoint(MapPoint.mapPointWithWCONGCoord(474300.0,1104123.0));
-						polyline2.addPoint(MapPoint.mapPointWithWCONGCoord(473873.0,1105377.0));
-						polyline2.addPoint(MapPoint.mapPointWithWCONGCoord(473302.0,1107097.0));
-						polyline2.addPoint(MapPoint.mapPointWithWCONGCoord(473126.0,1109606.0));
-						polyline2.addPoint(MapPoint.mapPointWithWCONGCoord(473063.0,1110548.0));
-						polyline2.addPoint(MapPoint.mapPointWithWCONGCoord(473435.0,1111020.0));
-						polyline2.addPoint(MapPoint.mapPointWithWCONGCoord(474068.0,1111714.0));
-						polyline2.addPoint(MapPoint.mapPointWithWCONGCoord(475475.0,1112765.0));
-						polyline2.addPoint(MapPoint.mapPointWithWCONGCoord(476938.0,1113532.0));
-						polyline2.addPoint(MapPoint.mapPointWithWCONGCoord(478725.0,1114391.0));
-						polyline2.addPoint(MapPoint.mapPointWithWCONGCoord(479453.0,1114785.0));
-						polyline2.addPoint(MapPoint.mapPointWithWCONGCoord(480145.0,1115145.0));
-						polyline2.addPoint(MapPoint.mapPointWithWCONGCoord(481280.0,1115237.0));
-						polyline2.addPoint(MapPoint.mapPointWithWCONGCoord(481777.0,1115164.0));
-						polyline2.addPoint(MapPoint.mapPointWithWCONGCoord(482322.0,1115923.0));
-						polyline2.addPoint(MapPoint.mapPointWithWCONGCoord(482832.0,1116322.0));
-						polyline2.addPoint(MapPoint.mapPointWithWCONGCoord(483384.0,1116754.0));
-						polyline2.addPoint(MapPoint.mapPointWithWCONGCoord(484401.0,1117547.0));
-						polyline2.addPoint(MapPoint.mapPointWithWCONGCoord(484893.0,1117930.0));
-						polyline2.addPoint(MapPoint.mapPointWithWCONGCoord(485016.0,1118034.0));
-						mapView.addPolyline(polyline2);
-						
-						mapView.fitMapViewAreaToShowAllPolylines();
-					}
-						break;
-					case 5: // Remove All Polylines
-					{
-						mapView.removeAllPolylines();
 					}
 						break;
 					}
@@ -373,8 +141,8 @@ public class MapActivity extends Activity implements
 	
 	public void onMapViewInitialized(MapView mapView) { 
 		Log.i(LOG_TAG, "MapView had loaded. Now, MapView APIs could be called safely"); 
+		// 어플리케이션을 처음 실행 시 지도를 현재 위치로 이동해준다
 		mapView.setCurrentLocationTrackingMode(MapView.CurrentLocationTrackingMode.TrackingModeOnWithoutHeading);
-		//mapView.setMapCenterPointAndZoomLevel(MapPoint.mapPointWithGeoCoord(37.537229,127.005515), 2, true);
 	} 
 	
 	@Override
@@ -396,15 +164,33 @@ public class MapActivity extends Activity implements
 	}
 
 	@Override
-	public void onMapViewLongPressed(MapView mapView, MapPoint mapPoint) {
+	public void onMapViewLongPressed(final MapView mapView, MapPoint mapPoint) {
+
+		final MapPoint.GeoCoordinate mapPointGeo = mapPoint.getMapPointGeoCoord();
 		
-		MapPoint.GeoCoordinate mapPointGeo = mapPoint.getMapPointGeoCoord();
+		String[] mapTypeMenuItems = {"정보 추가하기"};
 		
-		AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
-		alertDialog.setTitle("DaumMapLibrarySample");
-		alertDialog.setMessage(String.format("Long-Press on (%f,%f)", mapPointGeo.latitude, mapPointGeo.longitude));
-		alertDialog.setPositiveButton("OK", null);
-		alertDialog.show();
+		Builder dialog = new AlertDialog.Builder(this);
+		dialog.setTitle(TITLE);
+		dialog.setItems(mapTypeMenuItems, new OnClickListener() {
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				switch (which) {
+				case 0: // 위치 추가
+				{
+					poiItem = new MapPOIItem();
+					poiItem.setItemName("City on a Hill");
+					poiItem.setMapPoint(MapPoint.mapPointWithGeoCoord(mapPointGeo.latitude,mapPointGeo.longitude));
+					poiItem.setMarkerType(MapPOIItem.MarkerType.BluePin);
+					poiItem.setShowAnimationType(MapPOIItem.ShowAnimationType.DropFromHeaven);
+					mapView.addPOIItem(poiItem);
+				}
+					break;
+				}
+			}
+
+		});
+		dialog.show();
 	}
 
 	@Override
